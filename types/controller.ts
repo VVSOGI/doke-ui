302eabf367bc8da8ee4e066a811f40adc3df5a84
benny <vvsogi@gmail.com>
2025-03-04 17:18:26 +0900

    Add new files and types for folder structure

---
+++ types/controller.ts
@@ -0,0 +1,50 @@
+export interface Controller {
+  controllerName: string;
+  basePath: string;
+  description: string;
+  tags: string[];
+  endpoints: Endpoint[];
+}
+
+export interface Endpoint {
+  path: string;
+  method: string;
+  name: string;
+  description: string;
+  request: Request;
+  response: Response;
+}
+
+export interface DefaultProperty {
+  type: "string" | "boolean" | "number";
+  description: string;
+}
+
+export interface Body {
+  properties: Record<string, DefaultProperty>;
+  required: string[];
+}
+
+export interface Params {
+  properties: Record<string, DefaultProperty>;
+  required?: string[];
+}
+
+export interface Query {
+  properties: Record<string, DefaultProperty>;
+}
+
+type PropertyValueType = string | number | boolean;
+
+type ResponseExample = Record<string, PropertyValueType> | Record<string, PropertyValueType>[];
+
+export interface Request {
+  body?: Body;
+  query?: Query;
+  params?: Params;
+}
+
+export interface Response {
+  properties: Record<string, DefaultProperty>;
+  example: ResponseExample;
+}
