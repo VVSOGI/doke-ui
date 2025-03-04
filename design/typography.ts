c22e23ef38222427bc88f1933bee9936e0749c57
benny <vvsogi@gmail.com>
2025-03-04 18:18:14 +0900

    Add layout and typography configurations

---
+++ design/typography.ts
@@ -0,0 +1,20 @@
+import { ThemeConfig } from "tailwindcss/types/config";
+
+interface Typography {
+  fontSize: ThemeConfig["fontSize"];
+  fontFamily: ThemeConfig["fontFamily"];
+}
+
+export const typography: Typography = {
+  fontSize: {
+    0: ["0.75rem", { lineHeight: "1rem" }],
+    1: ["1rem", { lineHeight: "1.25rem" }],
+    2: ["1.25rem", { lineHeight: "1.5rem" }],
+    3: ["1.5rem", { lineHeight: "2rem" }],
+    4: ["2rem", { lineHeight: "2.5rem" }],
+    5: ["2.5rem", { lineHeight: "3rem" }],
+    6: ["3rem", { lineHeight: "3.5rem" }],
+  },
+
+  fontFamily: {},
+};
