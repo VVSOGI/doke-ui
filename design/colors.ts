8327fd21d577406cd2bdfe28610b2cca9923375e
benny <vvsogi@gmail.com>
2025-03-04 18:17:56 +0900

    Add design colors system and index files

---
+++ design/colors.ts
@@ -0,0 +1,37 @@
+import { ThemeConfig } from "tailwindcss/types/config";
+
+export const colors: ThemeConfig["colors"] = {
+  white: "#ffffff",
+  black: "#000000",
+  gray: {
+    100: "#edeff1",
+    200: "#e3e3e1",
+    300: "#d3d6da",
+    400: "#878a8c",
+    500: "#787c7e",
+  },
+
+  red: {
+    100: "#ff2e2e",
+    200: "#ca1d1d",
+    300: "#951414",
+  },
+
+  green: {
+    100: "#6aaa64",
+    200: "#538d4e",
+    300: "#468240",
+  },
+
+  blue: {
+    100: "#4a95f1",
+    200: "#3e7dcb",
+    300: "#2b588f",
+  },
+
+  yellow: {
+    100: "#f7da21",
+    200: "#c9b458",
+    300: "#b59f3b",
+  },
+};
