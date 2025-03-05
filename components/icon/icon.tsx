27ff79d160c3f9493b2c2fe7827edcbcebd2f8ca
benny <vvsogi@gmail.com>
2025-03-05 17:26:49 +0900

    Add Icon component

---
+++ components/icon/icon.tsx
@@ -0,0 +1,27 @@
+import { ICONS_LIST } from "@/lib/constants";
+
+interface props extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLSpanElement>, HTMLSpanElement> {
+  size?: string;
+  icons: ICONS_LIST;
+}
+
+/**
+ * @size Just enter the size you want in px.
+ * @icons You can call ICONS_LIST in /constants to select the available icons.
+ */
+export function Icon({ size = "20px", icons, className, ...props }: props) {
+  return (
+    <span
+      className={`
+        material-symbols-outlined
+        ${className}
+      `}
+      style={{
+        fontSize: size,
+      }}
+      {...props}
+    >
+      {icons}
+    </span>
+  );
+}
