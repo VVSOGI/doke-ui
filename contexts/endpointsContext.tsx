80525d84ce3ddcdf9d6545d93292069bc2d43c24
benny <vvsogi@gmail.com>
2025-03-05 18:19:15 +0900

    Add endpointsContext and index files

---
+++ contexts/endpointsContext.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { Controller } from "@/lib/types";
+import { createContext, useContext, ReactNode } from "react";
+
+interface EndpointsDataContextType {
+  endpointData: Controller[];
+}
+
+const EndpointsDataContext = createContext<EndpointsDataContextType | undefined>(undefined);
+
+interface EndpointsDataProviderProps {
+  children: ReactNode;
+  endpointData: Controller[];
+}
+
+export function EndpointsDataProvider({ children, endpointData }: EndpointsDataProviderProps) {
+  return <EndpointsDataContext.Provider value={{ endpointData }}>{children}</EndpointsDataContext.Provider>;
+}
+
+export function useEndpointData() {
+  const context = useContext(EndpointsDataContext);
+  if (context === undefined) {
+    throw new Error("useEndpointData must be used within an EndpointsDataProvider");
+  }
+  return context;
+}
