ff26d1845586c81fc4944bfd61ff96f6f9f7afaa
benny <vvsogi@gmail.com>
2025-03-06 17:22:28 +0900

    Update font and typography css settings

--- lib/design/typography.ts
+++ lib/design/typography.ts
@@ -3,18 +3,31 @@ import { ThemeConfig } from "tailwindcss/types/config";
 interface Typography {
   fontSize: ThemeConfig["fontSize"];
   fontFamily: ThemeConfig["fontFamily"];
+  fontWeight: ThemeConfig["fontWeight"];
 }
 
 export const typography: Typography = {
   fontSize: {
-    0: ["0.85rem", { lineHeight: "1rem" }],
-    1: ["1rem", { lineHeight: "1.25rem" }],
-    2: ["1.25rem", { lineHeight: "1.5rem" }],
-    3: ["1.5rem", { lineHeight: "2rem" }],
-    4: ["2rem", { lineHeight: "2.5rem" }],
-    5: ["2.5rem", { lineHeight: "3rem" }],
-    6: ["3rem", { lineHeight: "3.5rem" }],
+    0: ["0.65rem", { lineHeight: "1rem" }],
+    1: ["0.85rem", { lineHeight: "1rem" }],
+    2: ["1rem", { lineHeight: "1.25rem" }],
+    3: ["1.25rem", { lineHeight: "1.5rem" }],
+    4: ["1.5rem", { lineHeight: "2rem" }],
+    5: ["2rem", { lineHeight: "2.5rem" }],
+    6: ["2.5rem", { lineHeight: "3rem" }],
+    7: ["3rem", { lineHeight: "3.5rem" }],
   },
 
   fontFamily: {},
+  fontWeight: {
+    100: "100",
+    200: "200",
+    300: "300",
+    400: "400",
+    500: "500",
+    600: "600",
+    700: "700",
+    800: "800",
+    900: "900",
+  },
 };
