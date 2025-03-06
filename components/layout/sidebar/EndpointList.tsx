10534215a35d0c222dd6ce0ce0a1b76656d78575
benny <vvsogi@gmail.com>
2025-03-06 16:54:55 +0900

    Refactor activeEndpoints to activeController in EndpointList

--- components/layout/sidebar/EndpointList.tsx
+++ components/layout/sidebar/EndpointList.tsx
@@ -7,15 +7,15 @@ import { ICONS_LIST } from "@/lib/constants";
 
 export function EndpointList() {
   const { endpointData } = useEndpointData();
-  const [activeEndpoints, setActiveEndpoints] = useState([...endpointData.map((_, index) => index)]);
+  const [activeController, setActiveController] = useState([...endpointData.map((_, index) => index)]);
   const [currentIndex, setCurrentIndex] = useState("0-0");
 
   const onClickController = (index: number) => {
-    setActiveEndpoints((prev) => {
+    setActiveController((prev) => {
       if (prev.includes(index)) {
         return prev.filter((endpointIndex) => endpointIndex !== index);
       } else {
-        return [...activeEndpoints, index];
+        return [...activeController, index];
       }
     });
   };
@@ -31,9 +31,9 @@ export function EndpointList() {
           <div key={data.controllerName} className="flex flex-col gap-2">
             <ControllerButton onClick={() => onClickController(index)}>
               <div>{data.basePath.toUpperCase()}</div>
-              <Icon icons={activeEndpoints.includes(index) ? ICONS_LIST.ARROW_DROP_DOWN : ICONS_LIST.ARROW_DROP_UP} />
+              <Icon icons={activeController.includes(index) ? ICONS_LIST.ARROW_DROP_DOWN : ICONS_LIST.ARROW_DROP_UP} />
             </ControllerButton>
-            {activeEndpoints.includes(index) && (
+            {activeController.includes(index) && (
               <div className="flex flex-col gap-4">
                 {data.endpoints.map((endpoint, endpointIndex) => (
                   <EndpointButton
