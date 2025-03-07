c578de2bc8acd5af2e6c5acabc9df86d015e039a
benny <vvsogi@gmail.com>
2025-03-07 17:57:34 +0900

    Add smooth scroll behavior and adjust padding in ControllerPanel and PropertyTitle

--- components/title/PropertyTitle.tsx
+++ components/title/PropertyTitle.tsx
@@ -1,17 +1,18 @@
-import React from "react";
+import React, { HTMLAttributes } from "react";
 import { NotoSans } from "@/lib/assets";
 
-interface Props {
+interface Props extends HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode;
 }
 
-export function PropertyTitle({ children }: Props) {
+export function PropertyTitle({ children, ...props }: Props) {
   return (
     <div
       className={`
-          w-full border-b border-gray-200 pb-5 text-3 font-300 text-gray-500
-          ${NotoSans.className}
-        `}
+        w-full border-b border-gray-200 pb-5 text-3 font-300 text-gray-500
+        ${NotoSans.className}
+      `}
+      {...props}
     >
       {children}
     </div>
