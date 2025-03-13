97e4cc4858d195ad246c31dbdd5781029081f9b4
benny <vvsogi@gmail.com>
2025-03-13 18:58:31 +0900

    Refactor ExecutePanel component

--- components/layout/execute/ExecutePanel.tsx
+++ components/layout/execute/ExecutePanel.tsx
@@ -1,9 +1,8 @@
 "use client";
 
 import React, { memo, useEffect, useState } from "react";
-import { Icon, JsonView } from "@/components";
+import { CurlCommand, ExecuteHeader, ExecuteResponseExample } from "@/components";
 import { NotoSans } from "@/lib/assets";
-import { ICONS_LIST } from "@/lib/constants";
 import { Controller, DefaultProperty, Endpoint, Project } from "@/lib/types";
 import { processQueryParameters, processRequestBody, processUrlParameters } from "@/lib/utils/generateCurlCommand";
 
@@ -61,42 +60,15 @@ function Component({ projectData, controllerData, selected, setSelected }: Props
     >
       {selected && (
         <div>
-          <div className="w-full text-end p-7 pb-0">
-            <Icon
-              className={`
-                text-gray-100 cursor-pointer select-none
-                hover:text-gray-400
-                active:text-gray-200
-              `}
-              size="30px"
-              icons={ICONS_LIST.CLOSE}
-              onClick={() => setSelected(null)}
-            />
-          </div>
+          <ExecuteHeader onClick={() => setSelected(null)} />
           <div className={`flex flex-col gap-4 pb-8 px-11 ${NotoSans.className}`}>
             <div className="text-5 font-300 text-white">{selected.name}</div>
             <div className="flex gap-4 text-white text-3 font-300">
               <span>{selected.method}</span>
               <span>/{controllerData.basePath + selected.path}</span>
             </div>
-            <div className="flex flex-col gap-4 mt-4">
-              <div className={`text-white text-2 font-300`}>Example Request</div>
-              <div className="w-full h-fit p-8 bg-gray-800 rounded-sm text-white text-1 font-300 whitespace-pre-wrap">
-                {curlCommand}
-              </div>
-            </div>
-            <div className="flex flex-col gap-4">
-              <div className={`text-white text-2 font-300`}>Example Response</div>
-              <div className="w-full h-fit p-8 bg-gray-800 rounded-sm">
-                <JsonView
-                  src={selected.response.example}
-                  theme={"chalk"}
-                  style={{ backgroundColor: "transparent" }}
-                  displayObjectSize={false}
-                  enableClipboard={false}
-                />
-              </div>
-            </div>
+            <CurlCommand command={curlCommand} />
+            <ExecuteResponseExample endpoint={selected} />
           </div>
         </div>
       )}
