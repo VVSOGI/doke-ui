b54265555dd6091ea5c680c898ab04d385e6c827
benny <vvsogi@gmail.com>
2025-03-13 22:15:55 +0900

    Refactor CurlCommand component to improve readability

--- components/layout/execute/CurlCommand.tsx
+++ components/layout/execute/CurlCommand.tsx
@@ -9,9 +9,8 @@ function Component({ command, formattedBody }: Props) {
   return (
     <div className="flex flex-col gap-4 mt-4">
       <div className={`text-white text-2 font-400`}>Example Request</div>
-      <div className="w-full h-fit p-8 bg-gray-800 rounded-sm text-white text-1 font-300 whitespace-pre-wrap">
-        {command}
-        {formattedBody ? `-d '${formattedBody}'` : ""}
+      <div className="w-full h-fit p-8 bg-gray-800 rounded-sm text-white text-1 font-300 whitespace-pre-wrap break-all">
+        {formattedBody ? `${command}-d '${formattedBody}'` : command.slice(0, -3)}
       </div>
     </div>
   );
