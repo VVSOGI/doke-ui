af4bdf1586c97361fd5153f4adb14f86ca44d46d
benny <vvsogi@gmail.com>
2025-03-14 17:49:27 +0900

    Refactor CurlCommand component to use separate props for startCommand, headers, and formattedBody

--- components/layout/execute/CurlCommand.tsx
+++ components/layout/execute/CurlCommand.tsx
@@ -1,16 +1,19 @@
 import React, { memo } from "react";
 
 interface Props {
-  command: string;
+  startCommand: string;
+  headers: string;
   formattedBody: string | undefined;
 }
 
-function Component({ command, formattedBody }: Props) {
+function Component({ startCommand, headers, formattedBody }: Props) {
   return (
     <div className="flex flex-col gap-4 mt-4">
       <div className={`text-white text-2 font-400`}>Example Request</div>
       <div className="w-full h-fit p-8 bg-gray-800 rounded-sm text-white text-1 font-300 whitespace-pre-wrap break-all">
-        {formattedBody ? `${command}-d '${formattedBody}'` : command.slice(0, -3)}
+        {startCommand}
+        {headers && ` \\\n${headers}`}
+        {formattedBody && `-d ${formattedBody}`}
       </div>
     </div>
   );
