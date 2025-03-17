d886acfd49538edc700cadecad9c9608a72c1340
benny <vvsogi@gmail.com>
2025-03-17 17:39:21 +0900

    Delete CurlBodyProps and CurlParamProps, Add CurlProperties

---
+++ components/layout/execute/CurlProperties.tsx
@@ -0,0 +1,21 @@
+import React, { memo } from "react";
+import { CurlPropertyInput } from "@/components";
+
+interface Props {
+  title: string;
+  properties: Record<string, string>;
+  setProperties: (value: React.SetStateAction<Record<string, string> | undefined>) => void;
+}
+
+function Component({ title, properties, setProperties }: Props) {
+  return (
+    <div className="flex flex-col">
+      <div className="py-2 text-2 text-white border-b border-gray-300">{title}</div>
+      <div className="py-8 px-4">
+        <CurlPropertyInput properties={properties} setProperties={setProperties} />
+      </div>
+    </div>
+  );
+}
+
+export const CurlProperties = memo(Component);
