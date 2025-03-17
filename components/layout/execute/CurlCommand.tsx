a7354c0fb82a86c1e9cd954e1d0c9b53da365a53
benny <vvsogi@gmail.com>
2025-03-17 18:09:35 +0900

    Refactor CurlCommand component to include copy functionality

--- components/layout/execute/CurlCommand.tsx
+++ components/layout/execute/CurlCommand.tsx
@@ -1,4 +1,8 @@
-import React, { memo } from "react";
+"use client";
+
+import React, { memo, useEffect, useState } from "react";
+import { Icon } from "@/components";
+import { ICONS_LIST } from "@/lib/constants";
 
 interface Props {
   startCommand: string;
@@ -9,15 +13,47 @@ interface Props {
 }
 
 function Component({ startCommand, formattedParams, formattedQuerys, headers, formattedBody }: Props) {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const command =
+    startCommand +
+    formattedParams +
+    formattedQuerys +
+    (headers && ` \\\n${headers}`) +
+    (formattedBody && `-d '${formattedBody}'`);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsCopied(false);
+    }, 1500);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [isCopied]);
+
   return (
     <div className="flex flex-col gap-4">
-      <div className={`text-white text-2 font-400`}>Example Request</div>
+      <div className="flex justify-between text-white text-2 font-400">
+        <div>Example Request</div>
+        <div className="flex gap-4">
+          {isCopied && <div className="text-1 text-white">copied</div>}
+          <Icon
+            className={`
+            cursor-pointer select-none
+            hover:text-gray-300
+            active:text-gray-100
+          `}
+            onClick={() => {
+              navigator.clipboard.writeText(command);
+              setIsCopied(true);
+            }}
+            icons={ICONS_LIST.ASSIGNMENT}
+          />
+        </div>
+      </div>
       <div className="w-full h-fit p-8 bg-gray-800 rounded-sm text-white text-1 font-300 whitespace-pre-wrap break-all">
-        {startCommand}
-        {formattedParams}
-        {formattedQuerys}
-        {headers && ` \\\n${headers}`}
-        {formattedBody && `-d ${formattedBody}`}
+        {command}
       </div>
     </div>
   );
