8df49d4a312c0a2a417395654a9668477c4b08cd
benny <vvsogi@gmail.com>
2025-03-19 17:49:31 +0900

    Refactor API POST method to handle fetch errors

--- app/api/route.ts
+++ app/api/route.ts
@@ -8,12 +8,27 @@ interface NextCustomRequest extends NextRequest {
 export async function POST(req: NextCustomRequest) {
   const { serverUrl, endpoint, method, query, params, body } = await req.json();
 
-  const data = await fetch(serverUrl + endpoint + params + query, {
-    body: JSON.stringify(body),
-  });
+  try {
+    const response = await fetch(serverUrl + endpoint + params + query, {
+      method,
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+    const data = await response.json();
 
-  const response = await data.json();
-  console.log(response);
+    return NextResponse.json(data, { status: response.status });
+  } catch {
+    return NextResponse.json(
+      {
+        error: "fetch failed",
+        message: "The server encountered an unexpected condition that prevented it from fulfilling the request.",
+        statusCode: 500,
+      },
+      { status: 500 }
+    );
+  }
 
   return NextResponse.json({ data: "hello world!" });
 }
