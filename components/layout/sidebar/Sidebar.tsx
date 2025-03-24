3cff7cdfb145d04bd0a25cbbd06d089099e8bcb6
benny <vvsogi@gmail.com>
2025-03-24 17:28:59 +0900

    Update Sidebar component and add SidebarEndpointList component

--- components/layout/sidebar/Sidebar.tsx
+++ components/layout/sidebar/Sidebar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Divider, EndpointList, SidebarHeader } from "@/components";
+import { Divider, SidebarEndpointList, SidebarHeader } from "@/components";
 
 export function Sidebar() {
   return (
@@ -9,7 +9,7 @@ export function Sidebar() {
       <div className="w-full h-[calc(100%-2.25rem)] flex flex-col gap-6 py-8 px-8 overflow-scroll custom-scrollbar">
         <Divider>OVERVIEW</Divider>
         <Divider>ENDPOINTS</Divider>
-        <EndpointList />
+        <SidebarEndpointList />
       </div>
     </div>
   );
---
+++ components/layout/sidebar/SidebarEndpointList.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { useState } from "react";
+import { useActiveSection, useEndpointData } from "@/contexts";
+import { EndpointButton, ControllerButton } from "@/components";
+
+export function SidebarEndpointList() {
+  const { endpointData } = useEndpointData();
+  const { activeSection, changeSection } = useActiveSection();
+  const [activeController, setActiveController] = useState([...endpointData.map((_, index) => index)]);
+
+  const onClickController = (index: number) => {
+    setActiveController((prev) => {
+      if (prev.includes(index)) {
+        return prev.filter((endpointIndex) => endpointIndex !== index);
+      } else {
+        return [...activeController, index];
+      }
+    });
+  };
+
+  return (
+    <div className="flex flex-col gap-4">
+      {endpointData.map((data, index) => {
+        return (
+          <div key={data.controllerName} className="flex flex-col gap-4">
+            <ControllerButton isOpen={activeController.includes(index)} onClick={() => onClickController(index)}>
+              {data.basePath.toUpperCase()}
+            </ControllerButton>
+            {activeController.includes(index) && (
+              <div className="flex flex-col gap-4 mb-4">
+                {data.endpoints.map((endpoint) => (
+                  <EndpointButton
+                    key={endpoint.name}
+                    href={data.basePath + "#" + endpoint.name}
+                    active={endpoint.name === activeSection}
+                    onClick={() => changeSection(endpoint.name)}
+                  >
+                    {endpoint.name}
+                  </EndpointButton>
+                ))}
+              </div>
+            )}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
